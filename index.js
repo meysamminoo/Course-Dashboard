43ffdf13e813b05d30185ea196935ec54dc4da88
meysam <meisam.design.ph@gmail.com>
2022-02-20 16:36:16 +0330

    animation for progressbar

--- index.js
+++ index.js
@@ -11,22 +11,10 @@ function focusout() {
   search.style.width = "47rem";
 }
 
-// todo: progressbar
-var i = 0;
-function move() {
-  if (i == 0) {
-    i = 1;
-    var elem = document.querySelectorAll("myBar");
-    var width = 1;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(id);
-        i = 0;
-      } else {
-        width++;
-        elem.style.width = width + "%";
-      }
-    }
-  }
-}
+// todo: move element bottom to top
+window.addEventListener("DOMContentLoaded", function () {
+  const taskBox = document.querySelectorAll(".task-box");
+  taskBox.forEach((task) => {
+    task.classList.add("visible");
+  });
+});
